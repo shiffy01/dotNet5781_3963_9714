2311f564e79cebd41f8ba495c701b16a0e7f496e
shiffy01 <sscheine@g.jct.ac.il>
2020-10-26 17:34:28 +0200

    welcome9714

---
+++ dotNet5781_00_3963_9714/Program9714.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3963_9714
+{
+   partial class Program
+    {
+        static partial void welcome9714()
+        {
+            Console.WriteLine("i am also here");
+        }
+    }
+}
