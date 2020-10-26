bf1952610445e57f4138ab24934e5eb5a380e364
adina kallus <adina.ck.2000@gmail.com>
2020-10-26 17:12:53 +0200

    intial code+welcome3969

---
+++ dotNet5781_00_3963_9714/Program3963.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3963_9714
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome3963();
+            welcome9714();
+            Console.ReadKey();
+        }
+        static partial void welcome9714();
+        private static void welcome3963()
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console aplication", name);
+        }
+    }
+}
+
