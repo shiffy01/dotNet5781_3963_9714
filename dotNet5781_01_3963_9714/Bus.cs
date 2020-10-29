c13b08aa9a4d6d795f25d539be9dbec77b30a0e6
adina kallus <adina.ck.2000@gmail.com>
2020-10-29 15:43:13 +0200

    getters

---
+++ dotNet5781_01_3963_9714/Bus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_3963_9714
+{
+    class Bus
+    {
+        readonly int license;//מספר רישויpr
+         DateTime startDate;//תאריך תחילת הפעילות
+        double milage;//נסועה
+        double gas;//כמות דלק
+        bool dangerous;//אמת עם צריך טיפול, ושקר אחרת
+        public Bus(int licenceNumber, DateTime date)
+        {
+               
+        }
+        public int getLicense()
+        {
+            return license;
+        }
+        public DateTime getStartDate()
+        {
+            return startDate;
+        }
+        public double getMilage()
+        {
+            return milage;
+        }
+        public void setMilage(double newMilage)
+        {
+            if(newMilage>milage)//milage can only be increased
+
+        }
+        public double getGas()
+        {
+            return gas;
+        }
+    }
+}
