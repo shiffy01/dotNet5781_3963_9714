0a5a1be8b4cc565d01ffe47f8fbf835a94733e26
shiffy01 <sscheine@g.jct.ac.il>
2020-10-29 17:17:55 +0200

    hey

--- dotNet5781_01_3963_9714/Program.cs
+++ dotNet5781_01_3963_9714/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,29 @@ namespace dotNet5781_01_3963_9714
 {
     class Program
     {
-        static void Main(string[] args)
+        public static bool drive(int license, List<Bus> buses)
         {
-            drive
-            List<Bus> buses;
+            Bus bus1;
+            int i;
+            bool can;
+            for (i = 0; i < buses.Count; i++;)
+                {
+                    if (buses[i].getLicense() == license)
+                    { 
+                        bus1 = buses[i];
+                        Random rand = new Random(DateTime.Now.Millisecond);
+                        int num = rand.Next();
+                        can=bus1.SendTheBus(num);
+                    if(!can)
+                        Console.WriteLine("This bus is unsafe and therefore cannot do the job");
+                    return true;
+                    }
+                }
+            return false;
+        }
+            static void Main(string[] args)
+        {
+            List<Bus> buses = new List<Bus>();
             int num;
             Console.WriteLine("please choose one of the following options:");
             Console.WriteLine("1 to enter a new bus into the system");
@@ -27,23 +47,43 @@ namespace dotNet5781_01_3963_9714
                 switch (num)
                 {
                     case 1:
-                        Console.WriteLine("enter the license plate number:");
-                       int li = int.Parse(Console.ReadLine());
                         Console.WriteLine("enter year, month and day the bus started working");
                         int year = int.Parse(Console.ReadLine());
                         int month = int.Parse(Console.ReadLine());
                         int day = int.Parse(Console.ReadLine());
-                        DateTime newDate=new DateTime(year, month, day);
-                        Console.WriteLine("enter the mileage of the bus");
+                        DateTime date = new DateTime(year, month, day);
+                        Console.WriteLine("enter the license plate number:");
+                       int li = int.Parse(Console.ReadLine());
+                        if (date.Year < 2018)//if the bus bus made before 2018, its license must be 7 digits
+                            if (!(li > 9999999 || li < 1000000))//license is invalid
+                            {
+                                Console.WriteLine("Invalid. License must have 7 digits. Enter a new license plate number:");
+                                li = int.Parse(Console.ReadLine());
+                            }
+                            else//if the bus bus made after 2018, its license must be 8 digits
+                            {
+                                if (!(li > 99999999 || li < 10000000))//license is invalid
+                                    Console.WriteLine("Invalid. License must have 8 digits. Enter a new license plate number:");
+                                li = int.Parse(Console.ReadLine());
+                            }
+                        Console.WriteLine("enter mileage:");
                         int mi = int.Parse(Console.ReadLine());
-                        Bus bus1 = new Bus(li, newDate, mi);
+                        Console.WriteLine("enter year, month and day the bus had its last inspection");
+                        int year1 = int.Parse(Console.ReadLine());
+                        int month1 = int.Parse(Console.ReadLine());
+                        int day1 = int.Parse(Console.ReadLine());
+                        DateTime dateinspect = new DateTime(year, month, day);
+                        Bus bus1 = new Bus(li, date, mi, dateinspect);
                         buses.Add(bus1);
                         break;
                     case 2:
                         Console.WriteLine("enter the license plate number of the bus you want to use");
                         int lic = int.Parse(Console.ReadLine());
-
+                        bool worked = drive(lic, buses);
+                        if(!worked)
+                            Console.WriteLine("This bus does not exist in the company");
                         break;
+                    case 3:
 
                 } 
             }
