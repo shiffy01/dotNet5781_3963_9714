0cbee53edc1414dc0204eb1875c6fddc35835ccb
shiffy01 <sscheine@g.jct.ac.il>
2020-12-08 18:48:10 +0200

    capitals

--- dotNet5781_03B_3963_9714/MainWindow.xaml.cs
+++ dotNet5781_03B_3963_9714/MainWindow.xaml.cs
@@ -30,11 +30,11 @@ namespace dotNet5781_03B_3963_9714
             }
             //make one bus be passed the tune up date:
             DateTime date = new DateTime(01, 01, 2017);
-            buses[0].last_tune_up = date;
+            buses[0].Last_tune_up = date;
             //make one bus at least be close to the max milage allowed:
-            buses[1].total_milage = 19990;
+            buses[1].Total_milage = 19990;
             //make one bus with very little gas:
-            buses[3].gas = 10;
+            buses[3].Gas = 10;
         }
 
        
