648fd0ebc0127139925eb5b6be968db8015c942e
shiffy01 <sscheine@g.jct.ac.il>
2020-12-08 18:21:04 +0200

    initialized bus list

--- dotNet5781_03B_3963_9714/MainWindow.xaml.cs
+++ dotNet5781_03B_3963_9714/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using dotNet5781_01_3963_9714;
 
 namespace dotNet5781_03B_3963_9714
 {
@@ -20,8 +21,30 @@ namespace dotNet5781_03B_3963_9714
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+       private List<Bus> buses = new List<Bus>();//list of buses
+       void Initialize_bus_list()
         {
+            for (int i = 1; i < 11; i++)//add 10 buses to the list
+            {
+                buses.Add(new Bus(i, DateTime.Now, i));
+            }
+            //make one bus be passed the tune up date:
+            DateTime date = new DateTime(01, 01, 2017);
+            buses[0].last_tune_up = date;
+            //make one bus at least be close to the max milage allowed:
+            buses[1].total_milage = 19990;
+            //make one bus with very little gas:
+            buses[3].gas = 10;
+        }
+
+       
+
+
+
+
+    public MainWindow()
+        {
+           
             InitializeComponent();
         }
     }
