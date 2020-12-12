7174f823810421272c81226d67cbf1e2305a1bcf
shiffy01 <sscheine@g.jct.ac.il>
2020-12-12 23:56:43 +0200

    main window and details, but needs major fixing

---
+++ dotNet5781_03B_3963_9714/BusDetails.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using dotNet5781_01_3963_9714;
+
+namespace dotNet5781_03B_3963_9714
+{
+    /// <summary>
+    /// Interaction logic for BusDetails.xaml
+    /// </summary>
+    public partial class BusDetails : Window
+    {
+        public BusDetails(Bus b)
+        {
+            InitializeComponent();
+            grid1Bus.DataContext = b;
+        }
+
+      
+    }
+}
