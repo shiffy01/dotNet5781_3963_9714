a548172529dab74ad74451958da87bd79e51380c
shiffy01 <sscheine@g.jct.ac.il>
2020-12-24 14:01:18 +0200

    copied nurit's files

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BlApi;
+using BO;
 
-namespace UI
+
+namespace PlConsole
 {
     class Program
     {
+        static IBL bl;
+
         static void Main(string[] args)
         {
+            bl = BlFactory.GetBl();
+
+            Console.Write("Please enter how many days back: ");
+            int days = int.Parse(Console.ReadLine());
+            for (int d = days; d >= 0; --d)
+            {
+                Weather w = bl.GetWeather(d);
+                Console.WriteLine($"{d} days before - Feeling was: {w.Feeling} Celsius degrees");
+            }
+
         }
     }
 }
