6734b7ceaeac0572e0be0e08d5b018f8bce07dcc
shiffy01 <sscheine@g.jct.ac.il>
2020-12-29 20:30:06 +0200

    BO

---
+++ BL/BusLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+
+namespace BO
+{
+    class BusLine
+    {
+         int BusID {get; set;}
+         int Bus_line_number {get; set;}
+        IEnumerable<BusStation> Stations  {get; set;}
+        bool InterCity {get; set;}
+        string Destination {get; set;}
+        string Origin {get; set;}
+        public bool Exists{ get; set; }
+        DateTime First_bus{get; set;}
+        DateTime Last_bus{get; set;}
+        TimeSpan Frequency{get; set;}//time between each bus
+        public override string ToString()
+        {
+            return this.ToStringProperty();
+        }
+
+
+    }
+}
--- BL/BusStation.cs
+++ BL/BusStation.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DO;
 
-namespace DO
+
+
+namespace BO
 {
-    public class BusLineStation
+    class BusStation
     {
         int StationID {get; set;}
         public int Code{ get; set; }
-        int Number_on_route{ get; set; }
+        IEnumerable<BusLine> Lines{ get; set; }
+        double Latitude{ get; set; }
+        double Longitude{ get; set; }
+        string Street{ get; set; }
+        string City{ get; set; }
         public bool Exists{ get; set; }
         public override string ToString()
         {
