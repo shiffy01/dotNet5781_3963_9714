8cf64e332bee6d26409612a2118cbd7a25c1fd2d
shiffy01 <sscheine@g.jct.ac.il>
2020-12-30 21:57:09 +0200

    Starting DS

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -10,12 +10,38 @@ namespace DS
 {
     public static class DataSource
     {
-        public static List<WindDirection> directions;
-
-        static DataSource()
+        private static List<Bus> buses = new List<Bus>();//does this need to be an observerable collection? it does errors if i try..
+        public static List<Bus> Buses { get => buses; }
+        public static void initialize_buses()
         {
-            directions = new List<WindDirection>();
-            directions.Add(new WindDirection());
-        }
+            
+                    Buses.Add(new Bus {
+                    Status = Bus.Status_ops.Ready,
+                    License = 12345678,
+                    StartDate = DateTime.Now,
+                    Last_tune_up = DateTime.Now,
+                    TotalMilage = 0,
+                    Milage = 0,
+                    Gas = 0,
+                    HasDVD = false,
+                    HasWifi = false,
+                    IsAccessible = false
+
+
+
+                }) ;
+            Buses.Add(new Bus {
+                Status = Bus.Status_ops.Ready,
+                License = 1234567,
+                StartDate = DateTime.Now,
+                Last_tune_up = DateTime.Now,
+                TotalMilage = 0,
+                Milage = 0,
+                Gas = 0,
+                HasDVD = true,
+                HasWifi = false,
+                IsAccessible = true
+
+            }
     }
 }
