8811e98fbcce84dc836c26459a8d82f92b5e97be
adina kallus <adina.ck.2000@gmail.com>
2021-01-18 17:12:54 +0200

    data grid

--- PL/DisplayBusLines.xaml.cs
+++ PL/DisplayBusLines.xaml.cs
@@ -24,22 +24,37 @@ namespace PL
     {
 
         static IBL bl;
+        ObservableCollection<BusLine> Lines;
          void initialize_line_collection()
         {
             bl = BlFactory.GetBl();
-            IEnumerable<BusLine> stationIenumerable = bl.GetAllBusLines();
-            var stations = new ObservableCollection<BusLine>(stationIenumerable);
+            IEnumerable<BusLine> lineIenumerable = bl.GetAllBusLines();
+             Lines =new ObservableCollection<BusLine>(lineIenumerable);
         }//shallow copies from Ienumerable to observab
         public DisplayBusLines()
         {
             InitializeComponent();
             initialize_line_collection();
+            busLineDataGrid.DataContext = Lines;
         }
 
         private void add_line_Click(object sender, RoutedEventArgs e)
         {
             AddLine addLine = new AddLine();
             addLine.Show();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource busLineViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busLineViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busLineViewSource.Source = [generic data source]
+        }
+
+        private void busLineDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
