9760681472276c3313c66f1648e17e9202453670
adina kallus <adina.ck.2000@gmail.com>
2021-01-19 17:06:49 +0200

    update

--- PL/UpdateLine.xaml.cs
+++ PL/UpdateLine.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using BO;
 using BlApi;
 
+
 namespace PL
 {
     /// <summary>
@@ -25,11 +26,81 @@ namespace PL
     {
         static IBL bl;
         BusLine Line;
+        DateTime first;
+        DateTime last;
+        TimeSpan frequency;
+        int hr, min;
+        List<Time> hrs;
+        List<Time> mins;
         public UpdateLine(BusLine line)
         {
             bl = BlFactory.GetBl();
             InitializeComponent();
             Line = line;
+            bus_line_numberTextBox.DataContext = Line.Bus_line_number;
+            mins = new List<Time>();
+            hrs= new List<Time>();
+            first = Line.First_bus.Date;
+            last = Line.Last_bus.Date;
+            frequency = new TimeSpan();
+            
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource busLineViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busLineViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busLineViewSource.Source = [generic data source]
+        }
+
+      
+        private void bus_line_number_previewKeyDown(object sender, KeyEventArgs e)
+        {
+             TextBox text = sender as TextBox;
+                if (text == null) return;
+                if (e == null) return;
+
+               
+                if (e.Key == Key.Enter || e.Key == Key.Return || e.Key == Key.Tab)
+                    return;
+
+                //allow list of system keys (add other key here if you want to allow)
+                if (e.Key == Key.Escape || e.Key == Key.Back || e.Key == Key.Delete 
+                     || e.Key == Key.LeftShift || e.Key == Key.End || 
+                     e.Key == Key.Insert || e.Key == Key.Down || e.Key == Key.Right)
+                    return;
+
+                char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+
+                //allow control system keys
+                if (Char.IsControl(c)) return;
+
+                //allow digits (without Shift or Alt)
+                if (Char.IsDigit(c))
+                    if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt)))
+                        return; //let this key be written inside the textbox
+
+                //forbid letters and signs (#,$, %, ...)
+                e.Handled = true; //ignore this key. mark event as handled, will not be routed to other controls
+                return;
+            
+
+        }
+        private void bus_line_numberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateButton.IsEnabled = true;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+           // bl.UpdateBusLine()
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
