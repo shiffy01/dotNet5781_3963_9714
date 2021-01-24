e503169c99b738330ccc9aab6b26e266e9b16e8b
shiffy01 <sscheine@g.jct.ac.il>
2021-01-24 11:48:39 +0200

    back to data source

--- DLAPI/BusLine.cs
+++ DLAPI/BusLine.cs
@@ -10,15 +10,16 @@ namespace DO
     {
         public int BusID {get; set;}
         public int Bus_line_number {get; set;}
-        public bool Exists{ get; set; }
+       
         //public string Area { get; set; }
         //i added some more things here: i think theyre useful but check again
-        public bool InterCity {get; set;}
+      
         public string Destination {get; set;}
         public string Origin {get; set;}
         public DateTime First_bus{get; set;}
         public DateTime Last_bus{get; set;}
         public TimeSpan Frequency{get; set;}//time between each bus
+         public bool Exists{ get; set; }
         public override string ToString()
         {
             return this.ToStringProperty();
